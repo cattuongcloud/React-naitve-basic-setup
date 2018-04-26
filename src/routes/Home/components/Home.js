59671b38c87597ff32bab86d291c4c6aac9a8b8e
unknown <cattuong.cloud@gmail.com>
2018-04-26 14:19:11 +0700

    set up google map

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Container } from "native-base";
+import MapContainer from './MapContainer';
 class Home extends Component {
 	componentDidMount(){
 		this.props.setName(); 
-	}	
-	render() {   
+	}		
+	render() {
+		const region = {
+			latitude: 10.797113,
+			longitude:106.643596,
+			latitudeDelta:0.0922,
+			longitudeDelta:0.0421
+		}
 		return (
-		<View>
-			<Text>Hello Word {this.props.name}!</Text>   
-		</View>
+		<Container>
+			<MapContainer region={region}/>
+		</Container>
 		);
 	}
 }
